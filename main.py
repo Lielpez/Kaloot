d968bb775657222ea140d925a3ebdddbe600249b
liel <lielp2015@gmail.com>
2024-05-17 13:01:13 +0300

    added upload images feature

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
-from flask import Flask, render_template
-from os import listdir
-from os.path import isfile, join
-import random
+from flask import Flask, render_template, request, redirect, url_for
+from os import listdir, makedirs
+from os.path import isfile, join, splitext, exists
 import os
+import random
+import shutil
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/pictures'
+
+# Ensure the upload folder exists
+if not exists(UPLOAD_FOLDER):
+    makedirs(UPLOAD_FOLDER)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'files' not in request.files:
+            return 'No file part'
+        
+        files = request.files.getlist('files')
+        
+        # Clear the upload folder before saving new files
+        shutil.rmtree(UPLOAD_FOLDER)
+        makedirs(UPLOAD_FOLDER)
+
+        for file in files:
+            if file and file.filename != '':
+                file_name = os.path.basename(file.filename)
+                file.save(join(UPLOAD_FOLDER, file_name))
+        return redirect(url_for('game'))
+    
+    return render_template('upload.html')
+
 @app.route('/game')
 def game():
-    mypath=r'C:\Users\USER\Desktop\Kaloot\static\pictures'
+    mypath = UPLOAD_FOLDER
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    image_file=random.choice(onlyfiles)
-    options=[image_file]
+    
+    if not onlyfiles:
+        return 'No images found in the uploaded folder. Please upload images first.'
+    
+    image_file = random.choice(onlyfiles)
+    options = [image_file]
     onlyfiles.remove(image_file)
-    options+=(random.sample(onlyfiles, 3))
-    correct_answer=os.path.splitext(options[0])[0]
-    for i in range (len(options)):
-        options[i]=os.path.splitext(options[i])[0]
+    options += (random.sample(onlyfiles, 3))
+    correct_answer = splitext(options[0])[0]
+    
+    for i in range(len(options)):
+        options[i] = splitext(options[i])[0]
+    
     random.shuffle(options)
-    return render_template('game.html',options=options,image_file=image_file,correct_answer=correct_answer)
+    
+    return render_template('game.html', options=options, image_file=image_file, correct_answer=correct_answer)
 
 if __name__ == '__main__':
     app.run(debug=True)
